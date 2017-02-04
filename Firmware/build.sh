c2bf2fc9190028de4aacf17ab0b57dcffc613a8d
khalil estell <khalil.estell@sjsu.edu>
2017-02-03 16:02:08 -0800

    Updated README.md to be a tutorial on how to use SJSU-DEV-Linux. Updated build.sh to point to HelloWorld project by default, ENTITY_NAME to DBG and set the PATH variable to the local compiler. Removed some unnecessary files.

--- Firmware/build.sh
+++ Firmware/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-PROJECT_NAME=Bluetooth
-ENTITY_NAME=BLUETOOTH
+# Setup compiler file paths
+PATH=$PATH:$(pwd)/compiler/linux/bin
+
+# Setup Project Target
+PROJECT_NAME=HelloWorld
+ENTITY_NAME=DBG
 
 make -j8 PROJ=$PROJECT_NAME ENTITY=$ENTITY_NAME
